cfa99ece736e2567d9f7e652080fb8795737de67
root <roo@localhost.localdomain>
2018-08-20 22:28:16 +0900

    first commit

---
+++ openshift_dotnet_restfulapi/Controllers/TodoController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace kgy_Webapi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Todo")]
+    public class TodoController : Controller
+    {
+        // GET: api/Todo
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Todo/5
+        [HttpGet("{data}")]
+        public string Get(int data)
+        {
+            return "value";
+        }
+        
+        // POST: api/Todo
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/Todo/5
+        [HttpPut("{data}")]
+        public void Put(int data, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{data}")]
+        public void Delete(int data)
+        {
+        }
+    }
+}
---
+++ openshift_dotnet_restfulapi/README.md
@@ -0,0 +1,10 @@
+# openshift_dotent_restfulapi
+
+
+dotnet 2.0 use
+
+openshift type C# program
+
+Push hook test 1
+
+Push hook test 2
